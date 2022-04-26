ff17f7e7a3f3382f25f49be6d77a00b2dd8026e0
sm970309 <sm970309@gmail.com>
2022-04-26 16:34:01 +0900

    initial commit

--- App.js
+++ App.js
@@ -4,8 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Hello</Text>
+      <Text>I'm Sungmin</Text>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -17,4 +18,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    fontSize:28,
+    color:'red',
+  }
 });
