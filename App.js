208a63234a903a19d2549d12f44030ba3b728601
sm970309 <sm970309@gmail.com>
2022-04-27 14:45:54 +0900

    css스타일 추가

--- App.js
+++ App.js
@@ -3,23 +3,47 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello</Text>
-      <Text>I'm Sungmin</Text>
-      <StatusBar style="light" />
+    <View style={style.container}>
+      <StatusBar style='light'> </StatusBar>
+      <View style={style.city}>
+        <Text style={style.cityName}>SEOUL</Text>
+      </View>
+      <View style={style.wether}>
+        <View style={style.day}>
+          <Text style={style.temp}>27º</Text>
+          <Text style={style.description}>Sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1, backgroundColor: 'gold'
   },
-  text:{
-    fontSize:28,
-    color:'red',
+  city: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: "500"
+  },
+  wether: {
+    flex: 3,
+  },
+  day: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  temp:{
+    marginTop:10,
+    fontSize:178
+  },
+  description:{
+    marginTop:-30,
+    fontSize:48
   }
-});
+})
